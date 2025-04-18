fd00e0c6e051eaa2674f0f0b81c861ab4829465b
Admin <navr266@gmail.com>
2025-04-18 14:21:01 +0300

    book-service CRUD

---
+++ books-service/src/main/java/org/example/booksservice/BooksServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.booksservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooksServiceApplication.class, args);
+    }
+
+}
